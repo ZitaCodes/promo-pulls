bb54b5b8bcf6270e56c4a622973664c359620342
ZitaCodes <zita@zitacodes.tech>
2025-06-22 15:47:42 -0400

    Create pulls.js
    set up

---
+++ pulls.js
@@ -0,0 +1,6 @@
+import mongoose from "mongoose";
+import { send, json } from "micro";
+import Pull from "./models/Pull.js";
+import connectDB from "./utils/connectDB.js";
+
+// handler code from your previous working version...
